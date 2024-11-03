61836baa79fda77b15e42409f3dd04afec65fd38
grace <beko0002@hz.nl>
2024-11-03 20:23:26 +0100

    Trigger workflow rerun

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,17 @@
 // tests/app.test.js
 const request = require('supertest');
 const app = require('../src/app');
+let server;
+
+beforeAll(() => {
+  // Start the server before running tests
+  server = app.listen(3000);
+});
+
+afterAll((done) => {
+  // Close the server after all tests are done
+  server.close(done);
+});
 
 describe('GET /', () => {
   it('should respond with "Hello, World!"', async () => {
