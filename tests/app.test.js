81a546d2c1d8b5e81b694652ccd3b297074696a5
grace <beko0002@hz.nl>
2024-11-03 15:12:55 +0100

    Initial commit

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+// tests/app.test.js
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  it('should respond with "Hello, World!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, World!');
+  });
+});
